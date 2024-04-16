40c8a92defb17dcd2ca6030e71537af5f700fe9a
askaraliu <166808527+askaraliu@users.noreply.github.com>
2024-04-16 11:19:34 +0530

    Delete poll directory

--- poll/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from poll.models import Person
-# Register your models here.
-
-class PersonAdmin(admin.ModelAdmin):
-    list_display=('id','name','eid','dob','address','email','mob')
-
-admin.site.register(Person,PersonAdmin)
